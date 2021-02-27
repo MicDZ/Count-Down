0c23a2a20be7218df977dd27e707ba581d7cad10
MicDZ <me@micdz.cn>
2021-02-27 19:26:46 +0800

    upload.

---
+++ js/index.js
@@ -0,0 +1,35 @@
+window.addEventListener("load", () => {
+    const day = document.querySelector(".day>span");
+	const hour = document.querySelector(".hour>span");
+	const minute = document.querySelector(".minute>span");
+	const second = document.querySelector(".second>span");
+
+	const getTime = () => {
+
+        var EndTime= new Date('2022/06/07 08:00:00'); 
+        var NowTime = new Date();
+        var t =EndTime.getTime() - NowTime.getTime();
+       
+        const days = Math.floor(t/1000/60/60/24);
+
+
+		day.innerHTML = days < 100 ? (days<10? "00"+days: "0"+days) : days;        
+
+		const hours = Math.floor(t/1000/60/60%24);
+
+		hour.innerHTML = hours < 10 ? "0" + hours : hours;
+
+		const minutes = Math.floor(t/1000/60%60);
+		minute.innerHTML = minutes < 10 ? "0" + minutes : minutes;
+
+		const seconds = Math.floor(t/1000%60);
+		second.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+	};
+	getTime();
+
+	let timer = setInterval(() => {
+		getTime();
+	}, 1000);
+
+
+});
