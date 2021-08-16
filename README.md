9d615e0a2b96e2e95498e504859a005d40f06d72
MicDZ <me@micdz.cn>
2021-08-16 17:30:46 +0800

    Update.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # 2022高考倒计时
-![](https://t.micdz.cn/2022高考.png)
 
-本项目基于vue开发，由2019中考倒计时修改得来。
+本项目由2019中考倒计时修改得来
 
-感谢[Eqvpkbz](http://eqvpkbz.github.io/)大佬的帮助。
+感谢[Eqvpkbz](http://eqvpkbz.github.io/)大佬的帮助
+
+本项目的样式参考了 [https://gitee.com/QH_ayang/digital-time-unit](https://gitee.com/QH_ayang/digital-time-unit)
 
 ## URL参数
 
@@ -13,10 +14,13 @@
 
 * `name`为倒计时的标题
 
+* `mode`为倒计时的样式选择，格式为`light`或`night`
+
 参数之间用`&`分隔
 
 `date`、`time`、`name`任意缺少一个或目标时刻已过都会将倒计时自动切换为2022高考倒计时
 
+`mode`不设置则默认为`night`
 
 ## 使用实例
 
@@ -24,7 +28,7 @@
 
 [https://t.micdz.cn/?name=2022高考&date=2022/06/07&time=08:00](https://t.micdz.cn/?name=2022高考&date=2022/06/07&time=08:00)
 
-
+![](https://t.micdz.cn/2022高考.png)
 
 获取2021年高考倒计时
 
--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-var title = new Vue({
-    el: '#title',
-    data: {
-        title: '2022高考倒计时'
-    }
-})
-
-var clock = new Vue({
-    el: '#clock',
-    data: {
-        day: '000',
-        hour: '00',
-        minute: '00',
-        second: '00',
-        message: '距离2022年高考还有',
-        pass: '0.00%'
-    }
-})
-
-function change(a){
-    if (a == 1) {
-        css.href="css/night.css";
-    }
-    if (a == 2) {
-        css.href="css/light.css";
-    }
-}
-
-
 function getQueryVariable(variable){
 	let query = window.location.search.substring(1);
 	let vars = query.split("&");
@@ -36,39 +7,69 @@ function getQueryVariable(variable){
 	}
 	return(false);
 }
-if(getQueryVariable("name")) {
-    title.title = decodeURI(getQueryVariable("name")) + "倒计时";
-    clock.message = "距离" + decodeURI(getQueryVariable("name")) + "还有";
+
+window.addEventListener("load", () => {
+    const day = document.querySelector(".day");
+	const hour = document.querySelector(".hour");
+	const minute = document.querySelector(".minute");
+	const second = document.querySelector(".second");
+	
+	if(decodeURI(getQueryVariable("mode"))=="light") change(2);
+	else change(1);
+
+	const getTime = () => {
+		var EndTime=new Date("2022/06/07 08:00");
+		var name="2022高考";
+
+
+	
+		if(getQueryVariable("date")==false||getQueryVariable("time")==false||getQueryVariable("name")==false) EndTime= new Date("2022/06/07 08:00"),name="2022高考";
+		else EndTime= new Date(getQueryVariable("date")+" "+getQueryVariable("time")),name=decodeURI(getQueryVariable("name")); 
+
+		document.getElementById("name").innerHTML="距离"+decodeURI(name)+"还有";
+		document.getElementById("title").innerHTML=name+"倒计时";
+        var NowTime = new Date();
+        var t =EndTime.getTime() - NowTime.getTime();
+		
+		if(t<0) EndTime= new Date("2022/06/07 08:00"),name="2022高考",t =EndTime.getTime() - NowTime.getTime();
+
+        const days = Math.floor(t/1000/60/60/24);
+
+
+		day.innerHTML = days < 100 ? (days<10? "00"+days: "0"+days) : days;        
+
+		const hours = Math.floor(t/1000/60/60%24);
+
+		hour.innerHTML = hours < 10 ? "0" + hours : hours;
+
+		const minutes = Math.floor(t/1000/60%60);
+		minute.innerHTML = minutes < 10 ? "0" + minutes : minutes;
+
+		const seconds = Math.floor(t/1000%60);
+		second.innerHTML = seconds < 10 ? "0" + seconds : seconds;
+
+		var pass = Math.floor(((NowTime.getTime() / 1000) + 28800) % 86400) / 864;
+    	pass = pass.toFixed(2); 
+		document.getElementById("progress").value=pass;
+		document.querySelector(".data>span").innerHTML=pass+" % ";
+	};
+	getTime();
+
+	let timer = setInterval(() => {
+		getTime();
+	}, 1000);
+
+
+});
+function change(a){
+    var css=document.getElementById("css");
+    if (a == 1) {
+        css.setAttribute("href","css/night.css");
+    }
+    if (a == 2) {
+        css.setAttribute("href","css/light.css");
+    }
 }
-let EndTime = new Date("2022/06/07 08:00");
-if(getQueryVariable("date")==false||getQueryVariable("time")==false||getQueryVariable("name")==false) EndTime= new Date("2022/06/07 08:00"),name="2022高考";
-else EndTime= new Date(getQueryVariable("date")+" "+getQueryVariable("time")),name=decodeURI(getQueryVariable("name"));
-
-const getTime = () => {
-    let NowTime = new Date();
-    let t = EndTime.getTime() - NowTime.getTime();
-    if (t < 0) EndTime = new Date("2022/06/07 08:00"), name = "2022高考", t = EndTime.getTime() - NowTime.getTime();
-
-
-    const days = Math.floor(t / 1000 / 60 / 60 / 24);
-    clock.day = days < 100 ? (days < 10 ? "00" + days : "0" + days) : days;
-
-    const hours = Math.floor(t / 1000 / 60 / 60 % 24);
-    clock.hour = hours < 10 ? "0" + hours : hours;
-
-    const minutes = Math.floor(t / 1000 / 60 % 60);
-    clock.minute = minutes < 10 ? "0" + minutes : minutes;
-
-    const seconds = Math.floor(t / 1000 % 60);
-    clock.second = seconds < 10 ? "0" + seconds : seconds;
-
-    var pass = Math.floor(((NowTime.getTime() / 1000) + 28800) % 86400) / 864;
-    pass=pass.toFixed(2);
-    document.getElementById("progress").value=pass;
-    clock.pass=pass+" % ";
-}
-getTime();
-
-let timer = setInterval(() => {
-    getTime();
-}, 1000);
+function jump(){
+	window.location.href="https://github.com/MicDZ/Count-down";
+   }
